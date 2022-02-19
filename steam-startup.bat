0032905e2c440b9770e6f6e181a21f3179a148a3
ouzo12 <ouzo12@seamonkey.tja-data.dk>
2022-02-20 00:21:06 +0100

    added nobreak to timeout

--- steam-startup.bat
+++ steam-startup.bat
@@ -24,7 +24,7 @@ set username=!choice[%chose%]!
 c:
 cd C:\Program Files (x86)\Steam\
 start steam.exe -shutdown
-timeout 5
+timeout 5 /NOBREAK
 reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d %username% /f
 reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
 start steam://open/main
